73abfb13283597fd0df3fa829e2f678779780df2
Mmiiiimm <129563335+Mmiiiimm@users.noreply.github.com>
2024-02-25 21:16:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID","25281175"))
-API_HASH = getenv("API_HASH","6d99cb2b60a2c519fc1f99bd19565730")
+API_ID = int(getenv("API_ID","5019636942"))
+API_HASH = getenv("API_HASH","6d99cb2b60a2u519fc1f99bd19565730")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 480))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6581896306))
+OWNER_ID = int(getenv("OWNER_ID", 5019636942))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
