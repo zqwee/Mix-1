aa160a14027468a66137a1ac33874d78bf36ede0
Mmiiiimm <129563335+Mmiiiimm@users.noreply.github.com>
2024-02-27 23:58:29 +0200

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -59,7 +59,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text=". ReSuMe .", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text=". PaUsE .", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text=". RepLaY .", callback_data=f"ADMIN Replay|{chat_id}"),
-        ],[
+
             InlineKeyboardButton(text=". skip .", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text=". sToP .", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(text="- So MiiiiiX", url=f"https://t.me/P_T_I"),
